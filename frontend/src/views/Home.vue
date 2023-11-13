8d62fc0c60276c958f9030304e874a77ebdef1ed
lhb <lihaibao98@163.com>
2023-11-13 21:27:53 +0800

    [update] update home page with latest products

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -24,9 +24,17 @@
       <!-- 右侧内容区域 -->
       <div class="content">
         <!-- 你想展示的独立于其他页面的信息 -->
-        <div>
-          <h2>欢迎来到首页</h2>
-          <p>这里是一些专属于首页的信息。</p>
+        <div class="product-group">
+          <div v-for="(product, index) in latestProducts" :key="product.id" class="product-box">
+            <img :src="product.image" alt="Product Image">
+            <h3>{{ product.name }}</h3>
+            <p>介绍： {{ product.description }}</p>
+            <p>酒精度： {{ product.alcoholContent }}</p>
+            <p>原麦汁浓度： {{ product.originalGravity }}</p>
+            <p>价格: ￥{{ product.price.toFixed(2) }}</p>
+            <!-- 添加下面的判断，如果是每行的最后一个商品，则添加一个占位元素 -->
+            <div v-if="(index + 1) % productsPerRow === 0" class="filler"></div>
+          </div>
         </div>
         <!-- 其他内容 ... -->
         <router-view></router-view>
@@ -40,6 +48,7 @@ export default {
   data() {
     return {
       itemsPerRow: 1,
+      productsPerRow: 3,
       username: '用户1', // 替换为实际的用户名
       isSideBarHidden: false,
       navItems: [
@@ -54,6 +63,49 @@ export default {
         { id: 2, label: '我的订单', routeName: 'myOrders' },
         { id: 3, label: '我的收藏', routeName: 'myFavorites' },
         { id: 4, label: '我的地址', routeName: 'myAddresses' },
+      ],
+      latestProducts: [
+        {
+          id: 1,
+          name: '一世传奇',
+          image: new URL('@/static/legend.png', import.meta.url).href,
+          description: `
+            百位酿酒大师，传承百年工艺;
+            融汇百种酿法，经过千锤百炼;
+            酿于百年酒厂，藏于百年橡桶;
+            集合百味芳香，酿就百年传奇;
+            百酿成传奇……`,
+          alcoholContent: '>10.5% vol',
+          originalGravity: '23.9 P',
+          price: 1399.00,
+        },
+        {
+          id: 2,
+          name: '百年之旅',
+          image: new URL('@/static/bainian.png', import.meta.url).href,
+          description: `
+            总体印象:款有着微微焦糖、烤面包香气，有一定苦度。相当高的发酵程度。
+            香气: 麦芽香气较为明显，带有微微的焦甜、焦香香气。 较淡的酒花味道，包括柔和的花香。
+            外观: 琥珀色，酒液清亮通明，白色泡沫，持续时间较短。
+            味道: 随着从干爽到微甜香的变化，麦芽味道的感觉从低到明显，有微微的焦糖和烤面包香， 较淡的酒花苦味，带有辛辣，草药特点。
+            口感: 中度偏淡酒体，顺滑沙口。`,
+          alcoholContent: '6% vol',
+          originalGravity: '15 P',
+          price: 369,
+        },
+        {
+          id: 3,
+          name: '百年鸿运',
+          image: new URL('@/static/hongyunhead.jpg', import.meta.url).href,
+          description: `
+            青岛啤酒推出的一款限定款虎年生肖酒，选用优质大麦和上等啤酒花为原料酿制而成，
+            酒精度达到.5%vol，原麦汁浓度19.8°P，泡沫丰富，口感香醇；
+            该限定款啤酒采用喜庆吉祥的外观设计，包装使用红色礼盒，瓶身附带金虎图案，卡扣瓶盖更易开启。`,
+          alcoholContent: '5%',
+          originalGravity: '19.8 P',
+          price: 729,
+        },
+        // 添加更多新品...
       ],
     };
   },
@@ -75,4 +127,33 @@ export default {
 
 <style scoped>
 @import "../css/Home.css";
+
+.product-group {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap; /* 允许商品换行 */
+}
+
+.product-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* 让图片在水平方向上居中显示 */
+  border: 1px solid #ccc;
+  padding: 10px;
+  width: 30%; /* 每个商品框占据 30% 的宽度，以适应三列布局 */
+  margin: 10px;
+  text-align: left; /* 居左显示 */
+}
+
+.product-box img {
+  max-width: 100%;
+  height: auto;
+  align-self: center; /* 使图片在垂直方向上居中显示 */
+  margin-bottom: 10px; /* 添加一些底部间距 */
+}
+
+.filler {
+  flex: 1; /* 占满剩余空间 */
+}
+
 </style>
