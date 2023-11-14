74da4c69539be2a1977c34377edf9ba99cba1574
hbli <lihaibao98@163.com>
2023-11-14 15:53:47 +0800

    [add] add pinia to login

--- server/server.js
+++ server/server.js
@@ -40,7 +40,6 @@ app.post('/login', async (req, res) => {
     }
 
     const user = userResults[0];
-
     // 检查密码是否存在
     if (!user.password) {
       connection.release();
@@ -55,9 +54,13 @@ app.post('/login', async (req, res) => {
       connection.release();
       return res.status(401).json({ success: false, message: '密码错误' });
     }
-
+    console.log(user);
     connection.release();
-    return res.status(200).json({ success: true, message: '登录成功' });
+    return res.status(200).json({ 
+      success: true, 
+      message: '登录成功',
+      user: user 
+    });
   } catch (error) {
     console.error('登录失败', error);
     return res.status(500).json({ success: false, message: `登录失败: ${error.message || error}` });
